3de105f86104fa87e2042e8baa948324cd76cb38
MKcodeHub <135332465+MKcodeHub@users.noreply.github.com>
2024-01-15 21:16:26 -0800

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,6 @@
 import './Navbar.css';
+import websiteLogo from '../src/logo1.png'
+
 
 export default function Navbar () {
 
@@ -9,7 +11,7 @@ export default function Navbar () {
     return (
     <nav className='nav'>
       
-      <img onClick={() => window.location.href='/App.js'} className='webLogo' src='/logo1.png'></img>
+      <img onClick={() => window.location.href='/App.js'} className='webLogo' src={websiteLogo}></img>
       <a href="./App.js" className='site-title'>Wisdom.ST</a>
         <ul>
           {/* Slide Menu
